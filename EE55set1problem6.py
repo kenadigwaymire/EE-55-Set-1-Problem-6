e5955b3db18bd9ace85b5679a34d89e3bc658b34
kenadigwaymire <kwaymire@caltech.edu>
2023-10-05 00:44:31 -0700

    Updated code

--- EE55set1problem6.py
+++ EE55set1problem6.py
@@ -7,18 +7,22 @@ SMALL_QUANT_VAL = 0
 LARGE_QUANT_VAL = 255
 VARIANCE = [0.5, 5, 50]
 
-def quantizeImage(image_name: str):
+def loadImage(image_name: str):
     image = Image.open(image_name)
     pixels = np.asarray(image)
+    return pixels
+
+def quantizeImage(image_name: str):
+    pixels = loadImage(image_name)
     new_image = []
     for i in range(len(pixels)):
         new_image.append([])
-        for j in pixels[i]:
+        for j in range(len(pixels[i])):
             new_image[i].append(valCheck(pixels[i][j]))
     new_arr = np.array(new_image)
     new_img = Image.fromarray(new_arr)
-    img_name = "quantized_boat"
-    new_img.save(img_name, format="tiff")
+    new_image_name = "quantized_boat"
+    new_img.save(new_image_name, format="tiff")
     new_img.show()
 
 def valCheck(val: int):
@@ -29,13 +33,12 @@ def valCheck(val: int):
     return val
 
 def randomizeImage(image_name: str):
-    image = Image.open(image_name)
-    pixels = np.asarray(image)
+    pixels = loadImage(image_name)
     for x in VARIANCE:
         new_image = []
         for i in range(len(pixels)):
             new_image.append([])
-            for j in pixels[i]:
+            for j in range(len(pixels[i])):
                 new_image[i].append(pixels[i][j]+ np.random.normal(0, x))
         for x in range(len(new_image)):
             for y in new_image[x]:
@@ -45,6 +48,7 @@ def randomizeImage(image_name: str):
         img_name = "quantized_randomized_boat" + str(x)
         img.save(img_name, format="tiff")
         img.show()
+
 
 quantizeImage("boat.tiff")
 randomizeImage("boat.tiff")
